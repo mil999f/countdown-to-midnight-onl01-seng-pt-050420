40f51de3f546c956a0949d5b39a95294e91733ec
Milly Flores <miflo0h@gmail.com>
2020-05-15 16:13:19 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,20 @@
 #write your code here
 
-def countdown(n = 10)
-  loop do
-    break if n == 0
-    puts "#{n} SECONDS!" 
-    n -= 1
-   end
-  "HAPPY NEW YEAR!"
-  end
-end
+#def countdown(n = 10)
+ # loop do
+  #  break if n == 0
+   # puts "#{n} SECONDS!" 
+    #n -= 1
+   #end
+  #"HAPPY NEW YEAR!"
+  #end
+#end
 
+def countdown(number)
+  while number > 0
+    puts "#{number} SECONDS!"
+    number -= 1
+  end
+ puts "HAPPY NEW YEAR!"
+end
 
